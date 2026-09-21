e22641cd8ce393d2d1668c214b241fd66be08a8f
revaldi <bambangforlog1n@gmail.com>
2026-09-21 20:53:54 +0700

    pertemuan 3

--- README.md
+++ README.md
@@ -9,6 +9,12 @@
 
 ![pertemuan 1](images/pertemuan1.png)
 
-## Pertemuan 2
+## Pertemuan 2 
+
 ![pertemuan 2 (1)](images/pertemuan2.png) 
-![pertemuan 2 (2)](images/pertemuan2(2).png)
+![pertemuan 2 (2)](images/pertemuan2(2).png) 
+
+## Pertemuan 3
+
+![pertemuan 3 (1)](images/pertemuan3.png) 
+![pertemuan 3 (2)](images/pertemuan3(2).png)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.litert.metadata)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
